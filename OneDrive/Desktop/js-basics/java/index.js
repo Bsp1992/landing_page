22f98d0630e668284d6dd8fc730704469fed772b
Bsp1992 <epaymen@outlook.com>
2023-06-01 16:10:55 -0400

    Main

---
+++ OneDrive/Desktop/js-basics/java/index.js
@@ -0,0 +1,14 @@
+const names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
+const para = document.createElement('p');
+
+// Add your code here
+function chooseName(length, RNGUpperBound){
+  return new Array(length).fill().map(e => Math.round(Math.random() * RNGUpperBound))
+}
+console.log(chooseName);
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+
+section.appendChild(para);
+    
